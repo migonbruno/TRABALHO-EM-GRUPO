81b1bbf78f96c5c313a5ddc3552ffa118f32dfba
AlanSilva2813 <60469816+AlanSilva2813@users.noreply.github.com>
2020-02-06 13:36:14 -0300

    Add files via upload

---
+++ EXERCICIO 03/_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aprenda Git e GitHub!</title>
+    <link rel="stylesheet" href="css/template.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <a id="topo"></a>
+
+    <div class="wrap">
+            <nav class="nav">
+                <a href="index.php">Home</a>
+                <a href="git.php">Sobre o Git</a>
+                <a href="github.php">Sobre o GitHub</a>
+            </nav>      
+
+            <main class="main">
